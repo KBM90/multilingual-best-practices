38cf23845b5db346d6987b8e181dbfb10cd2de7a
KBM90 <36936973+KBM90@users.noreply.github.com>
2019-11-06 00:20:46 +0000

    The language file
    This file is just for testing

---
+++ ar.php
@@ -0,0 +1,18 @@
+<?php 
+
+
+      static $langArray = array(
+
+
+      	    'Login'          => 'تسجيل   الدخول',
+            'Admin' 		 => 'المدير',
+            'Page' 		     => 'Page',
+            'User ID' 		 => 'Identifian utilisateur',
+            'Password'       => 'Mot de passe',
+            "Dashboard"		 => "Contrôle"
+  
+      	  );
+
+
+
+ ?>
